aa82d3690a1702125a552fc8b727f6f1945c7889
soote <waffleee@gmail.com>
2014-03-31 10:36:59 -0400

    project

---
+++ src/granular/GranularSynth.java
@@ -0,0 +1,19 @@
+package granular;
+
+import granular.ui.GranularSynthesizer;
+
+/**
+ *
+ * @author soote
+ */
+public class GranularSynth {
+
+    public GranularSynth() {
+
+    }
+
+    public static void main(String[] args) {
+        new GranularSynthesizer().setVisible(true);
+    }
+
+}
